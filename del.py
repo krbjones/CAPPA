18332aced4885b84d33441bdf249d9a1dd390d9a
Kevin Jones <krbjones@gmail.com>
2014-02-19 21:32:56 -0500

    Create del.py

---
+++ del.py
@@ -0,0 +1,16 @@
+# FTP results to the Webserver
+import ftplib
+from ftplib import FTP 
+
+File2Del = "resultats.html"
+Output_Directory = "/public_html/ektejones.ca/cappa" 
+
+ftp = FTP("ftp.ekoot.ca")
+ftp.login("krbjones", "0Kl@h@m@") 
+file = open(File2Del, "rb") 
+ftp.cwd(Output_Directory)
+ftp.delete(File2Del) 
+print "File resultats.html was deleted" 
+ftp.quit() 
+file.close() 
+
