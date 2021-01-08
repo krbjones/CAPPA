105e2d129ea8525ce9353c7109e4255f7603dfff
Kevin Jones <krbjones@gmail.com>
2021-01-07 22:27:00 -0500

    Update del.py

--- del.py
+++ del.py
@@ -5,8 +5,8 @@ from ftplib import FTP
 File2Del = "resultats.html"
 Output_Directory = "/public_html/ektejones.ca/cappa" 
 
-ftp = FTP("ftp.ekoot.ca")
-ftp.login("krbjones", "0Kl@h@m@") 
+ftp = FTP("hostname")
+ftp.login("username", "password") 
 file = open(File2Del, "rb") 
 ftp.cwd(Output_Directory)
 ftp.delete(File2Del) 
